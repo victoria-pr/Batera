913c6f58247284779feefacfbfccb5feac2adcc3
victoria-pr <victoriaperojas@gmail.com>
2023-06-11 20:35:13 +0200

    empiezo a darle estilo al perfil del silver y corrijo import del modelo predictivo

--- frontend/src/components/PerfilSilverScreen.jsx
+++ frontend/src/components/PerfilSilverScreen.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import RecursosScreen from "./RecursosScreen";
 import Plot from "react-plotly.js";
+import "../screens.scss";
 import NavBar from "./NavBar";
 
 const PerfilSilverScreen = () => {
@@ -49,133 +50,138 @@ const PerfilSilverScreen = () => {
   return (
     <section className="silversection">
       <NavBar />
-      <div className="expediente">
-        <h1>Número Expediente</h1>
-        <h2>{data.silver_id}</h2>
-      </div>
-      <div className="perfil-container">
-        <div className="personaldata-card">
-          <h1>Datos personales</h1>
-          <div>
-            <h2>Nombre</h2>
-            <p>{data.name}</p>
-          </div>
+      <div className="perfil">
+        <div className="perfil-container">
+          <article className="perfil-article">
+            <h2>Número Expediente</h2>
+            <h2>{data.silver_id}</h2>
+          </article>
+          <h2>Datos personales</h2>
+          <article className="data-article">
+            {/* <div className="personaldata-card"> */}
+            <div className="info-card">
+              <h2>Nombre</h2>
+              <p>{data.name}</p>
+            </div>
 
-          <div>
-            <h2>Apellidos</h2>
-            <p>{data.surname}</p>
-          </div>
+            <div className="info-card">
+              <h2>Apellidos</h2>
+              <p>{data.surname}</p>
+            </div>
 
-          <div>
-            <h2>DNI/NIE</h2>
-            <p>{data.dni_nie}</p>
-          </div>
+            <div className="info-card">
+              <h2>DNI/NIE</h2>
+              <p>{data.dni_nie}</p>
+            </div>
 
-          <div>
-            <h2>Fecha de Nacimiento</h2>
-            <p>{data.birthday}</p>
-          </div>
+            <div className="info-card">
+              <h2>Fecha de Nacimiento</h2>
+              <p>{data.birthday}</p>
+            </div>
 
-          <div>
-            <h2>Número S.S</h2>
-            <p>{data.social_security_number}</p>
-          </div>
+            <div className="info-card">
+              <h2>Número S.S</h2>
+              <p>{data.social_security_number}</p>
+            </div>
 
-          <div>
-            <h2>Teléfono</h2>
-            <p>{data.telephone}</p>
-          </div>
+            <div className="info-card">
+              <h2>Teléfono</h2>
+              <p>{data.telephone}</p>
+            </div>
 
-          <div>
-            <h2>Correo electrónico</h2>
-            <p>{data.email}</p>
-          </div>
+            <div className="info-card">
+              <h2>Correo electrónico</h2>
+              <p>{data.email}</p>
+            </div>
 
-          <div>
-            <h2>Domicilio</h2>
-            <p>{data.address}</p>
-          </div>
+            <div className="info-card">
+              <h2>Domicilio</h2>
+              <p>{data.address}</p>
+            </div>
 
-          <div>
-            <h2>Localidad</h2>
-            <p>{data.city}</p>
-          </div>
+            <div className="info-card">
+              <h2>Localidad</h2>
+              <p>{data.city}</p>
+            </div>
 
-          <div>
-            <h2>CP</h2>
-            <p>{data.postal_code}</p>
-          </div>
+            <div className="info-card">
+              <h2>CP</h2>
+              <p>{data.postal_code}</p>
+            </div>
 
-          <div className="contactdata-card"></div>
-          <h1>Persona de contacto</h1>
-          <div>
-            <h2>Nombre</h2>
-            <p>{data.contact_person}</p>
-          </div>
+            <article className="data-article">
+              <h2>Persona de contacto</h2>
+              <div>
+                <h2>Nombre</h2>
+                <p>{data.contact_person}</p>
+              </div>
 
-          <div>
-            <h2>Parentesco</h2>
-            <p>{data.contact_p_relation}</p>
-          </div>
+              <div>
+                <h2>Parentesco</h2>
+                <p>{data.contact_p_relation}</p>
+              </div>
 
-          <div>
-            <h2>Teléfono</h2>
-            <p>{data.contact_p_telephone}</p>
-          </div>
-        </div>
-      </div>
-      <div className="valoration-card">
-        <h1>VALORACIONES REALIZADAS</h1>
-        <div className="valoration">
-          {data.loneliness_forms?.map((loneliness, index) => {
-            return (
-              <Link
-                key={index}
-                to={`/formularioSilver/${loneliness.lon_form_id}`}
-                className="form-card"
-              >
-                <h2>{loneliness.loneliness_id}</h2>
-                <h2>{loneliness.date}</h2>
+              <div>
+                <h2>Teléfono</h2>
+                <p>{data.contact_p_telephone}</p>
+              </div>
+            </article>
+            {/*  </div> */}
+          </article>
+          <article className="perfil-article">
+            <h2>VALORACIONES REALIZADAS</h2>
+            <div className="valoration-container">
+              {data.loneliness_forms?.map((loneliness, index) => {
+                return (
+                  <Link
+                    key={index}
+                    to={`/formularioSilver/${loneliness.lon_form_id}`}
+                    className="valoration-card"
+                  >
+                    <h2>{loneliness.loneliness_id}</h2>
+                    <h2>{loneliness.date}</h2>
+                  </Link>
+                );
+              })}
+            </div>
+            <div className="newvaloration">
+              <Link to={`/formularioSilver/${data.silver_id}/create`}>
+                <button>Nueva Valoración</button>
               </Link>
-            );
-          })}
+            </div>
+          </article>
+          <div className="graphic-card">
+            <h1>Gráfico</h1>
+            <Plot
+              data={[
+                {
+                  type: "scatter",
+                  mode: "lines+markers",
+                },
+                {
+                  type: "bar",
+                  x: [1, 2, 3],
+                  y: suma,
+                  marker: {
+                    color: "#0A7F8D",
+                  },
+                  text: suma,
+                  textposition: "auto",
+                  hoverinfo: "none",
+                },
+              ]}
+              layout={{
+                width: 420,
+                height: 340,
+                title: "Evolución valoraciones",
+                xaxis: {
+                  tickmode: "array",
+                  tickvals: [1, 2, 3],
+                },
+              }}
+            />
+          </div>
         </div>
-        <div className="newvaloration">
-          <Link to={`/formularioSilver/${data.silver_id}/create`}>
-            <button>Nueva Valoración</button>
-          </Link>
-        </div>
-      </div>
-      <div className="graphic-card">
-        <h1>Gráfico</h1>
-        <Plot
-          data={[
-            {
-              type: "scatter",
-              mode: "lines+markers",
-            },
-            {
-              type: "bar",
-              x: [1, 2, 3],
-              y: suma,
-              marker: {
-                color: "#0A7F8D",
-              },
-              text: suma,
-              textposition: "auto",
-              hoverinfo: "none",
-            },
-          ]}
-          layout={{
-            width: 420,
-            height: 340,
-            title: "Evolución valoraciones",
-            xaxis: {
-              tickmode: "array",
-              tickvals: [1, 2, 3],
-            },
-          }}
-        />
       </div>
     </section>
   );
