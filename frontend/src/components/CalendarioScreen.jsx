2cd17c929bdbce42ca3619fb069e3cae71a7835d
victoria-pr <victoriaperojas@gmail.com>
2023-06-12 23:47:09 +0200

    cambiado el formController para poder acceder a los ultimos recursos asignados. Agregados los iconos del navBar nuevo. Aplicados estilo, alguno sin acabar, al navBar, PerfilSilverScreen, Tareas, entre otros

--- frontend/src/components/CalendarioScreen.jsx
+++ frontend/src/components/CalendarioScreen.jsx
@@ -3,20 +3,27 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import esLocale from "@fullcalendar/core/locales/es";
 import "../style.scss";
+import NavBarIcons from "./NavBarIcons";
 
 function Calendario() {
   const eventos = [
     {
-      title: "Valoraciones",
-      start: "2023-06-01",
-      end: "2023-06-16",
-      note: "Reagendadas",
+      title: "Reunión",
+      start: "2023-06-16",
+      note: "personal",
     },
     {
-      title: "Evento 2",
-      start: "2023-06-05",
-      note: "Nota para el evento 2",
+      title: "Videollamada",
+      start: "2023-06-16",
+      note: "integración",
+    },
+    {
+      title: "Valoraciones",
+      start: "2023-06-12",
+      end: "2023-06-17",
+      note: "",
     },
   ];
 
@@ -31,21 +38,25 @@ function Calendario() {
   };
 
   return (
-    <div className="calendario-container">
-      <div className="calendario">
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
-          headerToolbar={{
-            start: "today prev,next",
-            center: "title",
-            end: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-          events={eventos}
-          eventContent={renderEventContent}
-        />
+    <section>
+      <NavBarIcons />
+      <div className="calendario-container">
+        <div className="calendario">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+              start: "today prev,next",
+              center: "title",
+              end: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            events={eventos}
+            eventContent={renderEventContent}
+            locale={esLocale}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 export default Calendario;
--- frontend/src/components/PerfilScreen.jsx
+++ frontend/src/components/PerfilScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import NavBarIcons from "./NavBarIcons";
 
 function Perfil() {
   const [loged, setLoged] = useState(localStorage.getItem("infoUser"));
@@ -12,6 +13,7 @@ function Perfil() {
   };
   return (
     <div>
+      <NavBarIcons />
       Perfil
       {loged && (
         <button className="logOut" onClick={logout}>
