abf5210babee315b43521661110402c9fc1a0a51
Anderwander <ander1993trabajo@hotmail.com>
2023-06-14 14:10:14 +0200

    modelo reentrena

--- frontend/src/components/ModeloScreen.jsx
+++ frontend/src/components/ModeloScreen.jsx
@@ -7,6 +7,22 @@ const ModeloScreen = ({ data }) => {
   const [modelo, setModelo] = useState();
   const [showLastResource, setShowLastResource] = useState(false);
   const navigate = useNavigate();
+  const [lastRecurso, setLastRecurso] = useState({
+    resource: {
+      board_games: null,
+      cofee_n_chat: null,
+      cooking_group: null,
+      cycling_group: null,
+      day_care_center: null,
+      garden_group: null,
+      home_assistance: null,
+      phone_assistance: null,
+      movie_club: null,
+      reading_club: null,
+      walking_club: null,
+    },
+  });
+
   const infoUser = localStorage.getItem("infoUser");
   const localUser = JSON.parse(infoUser);
   console.log("Modeloorl", modelo);
@@ -116,30 +132,144 @@ const ModeloScreen = ({ data }) => {
     }
   };
 
-  const [lastRecurso, setLastRecurso] = useState({});
+  //retrain modelo predictivo
+  const retrainModelo = async () => {
+    try {
+      //espera a que esté lastRecurso
+      const lastRecursoResources = [];
+      if (lastRecurso.resource.board_games === "1") {
+        lastRecursoResources.push("Juegos de mesa");
+      }
+      if (lastRecurso.resource.cofee_n_chat === "1") {
+        lastRecursoResources.push("Café y charlas");
+      }
+      if (lastRecurso.resource.cooking_group === "1") {
+        lastRecursoResources.push("Grupo de cocina");
+      }
+      if (lastRecurso.resource.cycling_group === "1") {
+        lastRecursoResources.push("Grupo de ciclistas");
+      }
+      if (lastRecurso.resource.day_care_center === "1") {
+        lastRecursoResources.push("Centro de día");
+      }
+      if (lastRecurso.resource.garden_group === "1") {
+        lastRecursoResources.push("Grupo de jardinería");
+      }
+      if (lastRecurso.resource.home_assistance === "1") {
+        lastRecursoResources.push("Asistencia domiciliaria");
+      }
+      if (lastRecurso.resource.phone_assistance === "1") {
+        lastRecursoResources.push("Asistencia telefónica");
+      }
+      if (lastRecurso.resource.movie_club === "1") {
+        lastRecursoResources.push("Club de cine");
+      }
+      if (lastRecurso.resource.reading_club === "1") {
+        lastRecursoResources.push("Club de lectura");
+      }
+      if (lastRecurso.resource.walking_club === "1") {
+        lastRecursoResources.push("Grupo de caminar");
+      }
+      // Agrega más condicionales según sea necesario para las demás propiedades
+      console.log("A VER QUE ME DA", lastRecurso.resource.board_games);
+      const recursos_ut = lastRecursoResources.join(",");
+      console.log("ESTO ES LASTRECURSO", lastRecursoResources);
+
+      const url = `https://jonellacuria.pythonanywhere.com/v2/retrain?Suma=${
+        data.sum
+      }&ID_Agente=${localUser.id}&Edad=${edad}&Sexo_Femenino=${
+        data.silver?.gender === "Femenino" ? "1" : "0"
+      }&Sexo_Masculino=${
+        data.silver?.gender === "Masculino" ? "1" : "0"
+      }&Municipio_Amorebieta_Etxano=${
+        data.silver?.city === "Amorebieta-Etxano" ? "1" : "0"
+      }&Municipio_Arratia=${
+        data.silver?.city === "Arratia" ? "1" : "0"
+      }&Municipio_Barakaldo=${
+        data.silver?.city === "Barakaldo" ? "1" : "0"
+      }&Municipio_Basauri_Etxebarri=${
+        data.silver?.city === "Basauri/Etxebarri" ? "1" : "0"
+      }&Municipio_Bermeo=${
+        data.silver?.city === "Bermeo" ? "1" : "0"
+      }&Municipio_Bilbao=${
+        data.silver.city === "Bilbao" ? "1" : "0"
+      }&Municipio_Busturialdea=${
+        data.silver?.city === "Busturialdea" ? "1" : "0"
+      }&Municipio_Durango=${
+        data.silver?.city === "Durango" ? "1" : "0"
+      }&Municipio_Erandio=${
+        data.silver?.city === "Erandio" ? "1" : "0"
+      }&Municipio_Ermua_Mallabia=${
+        data.silver?.city === "Ermua/Mallabia" ? "1" : "0"
+      }&Municipio_Galdakao=${
+        data.silver?.city === "Galdakao" ? "1" : "0"
+      }&Municipio_Getxo=${
+        data.silver?.city === "Getxo" ? "1" : "0"
+      }&Municipio_Goi_Enkarterri=${
+        data.silver?.city === "Goi Enkarterri" ? "1" : "0"
+      }&Municipio_Lea_Artibai=${
+        data.silver?.city === "Lea Artibai" ? "1" : "0"
+      }&Municipio_Leioa=${
+        data.silver?.city === "Leioa" ? "1" : "0"
+      }&Municipio_Meatzaldea=${
+        data.silver?.city === "Meatzaldea" ? "1" : "0"
+      }&Municipio_Mungialde=${
+        data.silver?.city === "Mungialde" ? "1" : "0"
+      }&Municipio_Nerbioi=${
+        data.silver?.city === "Nerbioi" ? "1" : "0"
+      }&Municipio_Portugalete=${
+        data.silver?.city === "Portugalete" ? "1" : "0"
+      }&Municipio_Santurtzi=${
+        data.silver?.city === "Santurtzi" ? "1" : "0"
+      }&Municipio_Sestao=${
+        data.silver?.city === "Sestao" ? "1" : "0"
+      }&Municipio_Trasladados=${
+        data.silver?.city === "Trasladados" ? "1" : "0"
+      }&Municipio_Txorierri=${
+        data.silver?.city === "Txorierri" ? "1" : "0"
+      }&Municipio_Uribe_Kosta=${
+        data.silver?.city === "Uribe Kosta" ? "1" : "0"
+      }&Estado_Civil_Casado=${
+        data.silver?.marital_status === "Casado/a" ? "1" : "0"
+      }&Estado_Civil_Divorciado=${
+        data.silver?.marital_status === "Divorciado/a" ? "1" : "0"
+      }&Estado_Civil_Soltero=${
+        data.silver?.marital_status === "Soltero/a" ? "1" : "0"
+      }&Estado_Civil_Viudo=${
+        data.silver?.marital_status === "Viudo/a" ? "1" : "0"
+      }&recursos_ut=${recursos_ut}&Suma_ant=${
+        data.silver.loneliness_forms.slice(-2)[0].sum
+      }`; //Tener en cuenta esta ruta
+      console.log("la url que pasamos para reentrenar: ", url);
+      const response = await axios.put(url); //probar con put
+      console.log("la respuesta de reentrenar: ", response);
+    } catch (error) {
+      console.log("Da este error al reentrenar", error);
+      if (error.response.status === 401 || error.response.status === 400) {
+        console.log("error 401 el if del catch");
+      }
+    }
+  };
+
   const obtenerUltimoRecurso = async () => {
-    setLastRecurso(data.silver.loneliness_forms.slice(-1)[0]);
-    if (data.silver.loneliness_forms.slice(-1)[0].resource === null) {
-      setLastRecurso(data.silver.loneliness_forms.slice(-2)[0]);
-    }
-    if (
-      data.silver.loneliness_forms.slice(-1)[0].resource === null &&
-      data.silver.loneliness_forms.slice(-2)[0].resource === null
-    ) {
-      setLastRecurso(data.silver.loneliness_forms.slice(-3)[0]);
-    }
-    if (
-      data.silver.loneliness_forms.slice(-1)[0].resource === null &&
-      data.silver.loneliness_forms.slice(-2)[0].resource === null &&
-      data.silver.loneliness_forms.slice(-3)[0].resource === null
-    ) {
-      setLastRecurso(data.silver.loneliness_forms.slice(-4)[0]);
+    if (data.silver.loneliness_forms.length > 0) {
+      const lastForm =
+        data.silver.loneliness_forms[data.silver.loneliness_forms.length - 2];
+      if (lastForm.resource === null) {
+        const previousForm =
+          data.silver.loneliness_forms[data.silver.loneliness_forms.length - 3];
+        setLastRecurso(previousForm);
+        setShowLastResource(true);
+      } else {
+        setLastRecurso(lastForm);
+        setShowLastResource(true);
+      }
     }
   };
 
   useEffect(() => {
     obtenerUltimoRecurso();
-  }, []);
+  }, [data]);
 
   return (
     <article className="modeloscreen">
@@ -150,6 +280,8 @@ const ModeloScreen = ({ data }) => {
               className="predict-button"
               onClick={() => {
                 obtenerPrediccion();
+                retrainModelo();
+                console.log("el ultimo recurso: ", lastRecurso);
               }}
             >
               Obtener recomendación
